12e05b669678348b82bdcdf86e6b26572fe913bb
manoylo <yuriy.manoylo@gmail.com>
2015-10-16 20:22:20 +0300

    server side rendering started

--- api/controllers/QuestionController.js
+++ api/controllers/QuestionController.js
@@ -6,6 +6,33 @@
  */
 
 module.exports = {
-	
+  index: function (req, res) {
+    Question.find()
+      .sort('createdAt DESC')
+      .exec(function (err, questions) {
+        if (err) {
+          return res.send(500);
+        }
+        res.view({
+          questions: questions
+        });
+      });
+  },
+
+  view: function (req, res) {
+    Question.findOne({
+      id: req.params['questionId']
+    }).exec(function (err, question) {
+      if (err) {
+        return res.send(500);
+      }
+      if(!question) {
+        return res.notFound();
+      }
+      res.view({
+        q: question
+      });
+    });
+  }
 };
 
